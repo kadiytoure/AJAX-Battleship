2a66951a14cf512271294f72d842ea9f922245ad
toure <tourekadiy@gmail.com>
2017-07-27 16:33:03 +0200

    Mise à jour du tableau avec  les lignes(y) et les colonnes(x);

---
+++ game.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+<?php 
+    $compteur = 0;
+    // première boucle pour les tr (lignes)
+
+
+    for($x = 0; $x <= 10; $x++) {
+        echo '<tr>';
+        //et une seconde boucle pour les td (colonnes)
+        for ($y= 0; $y <= 10; $y++) {
+            $compteur++;
+            echo '<td>(' . $x . ', ' . $y . ')</td>';
+        }
+        //en oubliant pas de fermer les tr
+        echo '</tr>';
+    }?>
+</table>
+<script>
+let boat = [];
+let ligne = document.querySelectorAll("tr");
+for(let y = 0; y < ligne.length; y = y + 1){
+    let column = ligne[y].children;
+    for(let x = 0; x < column.length; x = x + 1) {
+        column[x].addEventListener("click", function(e) {
+            let b = {
+                "x": x,
+                "y": y,
+            };
+            console.log(b);
+            boat.push(b);
+            column[x].style.background = 'green';
+            e.preventDefault();
+        });
+    }
+}
+
+</script>
+</body>
+</html>
+<!-- Afficher les bateaux avec leurs noms et leurs nbres de cases; -->
